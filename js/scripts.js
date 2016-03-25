228185343792d93d3dccca311ad668aa7e3f5dfc
Kyle Wolfson <wolfsonk@gmail.com>
2016-03-25 15:03:05 -0700

    have individual destination pictures

--- js/scripts.js
+++ js/scripts.js
@@ -10,71 +10,63 @@ $(document).ready(function() {
     var result
 
     if (travel && geography && density && price && temperature){
-      $("#inputError").hide()
+      $(".picsize").fadeOut(500);
+      $("#inputError").hide();
       $(".has-error").removeClass("has-error"); //successful inputs, clears fields and headline
       // begin excessive nested if statements
       if (travel === "domestic") {
-      $("#foreignResult").fadeOut(500)
-      $("#domesticResult").fadeOut(500)
-      setTimeout(function() {
-        $("#domesticResult").fadeIn(500);
-      }, 500)
-        if (geography === "water") {
-          $("#waterPic").fadeOut(500)
-          $("#mountainPic").fadeOut(500)
-          setTimeout(function() {
-            $("#waterPic").fadeIn(500);
-          }, 500)
-          if (density === "city") {
-            if (price === "cheap") {
-              if (temperature === "hot") {result = "Tallahassee" }
-              else {result = "Buffalo"}
-            }
-            else { //classy
-              if (temperature === "hot") {result = "Los Angeles" }
-              else {result = "New York"}
-            }
-          }
+        $("#foreignResult").fadeOut(500)
+        $("#domesticResult").fadeOut(500)
+        setTimeout(function() {
+          $("#domesticResult").fadeIn(500);
 
-          else { //rural
-            if (price === "cheap") {
-              if (temperature === "hot") {result = "the Everglades" }
-              else {result = "the Allagash Wilderness"}
+          if (geography === "water") {
+            if (density === "city") {
+              if (price === "cheap") {
+                if (temperature === "hot") {$("#tallahasseePic").show(), result = "Tallahassee" }
+                else {$("#buffaloPic").show(), result = "Buffalo"}
+              }
+              else { //classy
+                if (temperature === "hot") {$("#LAPic").show(), result = "Los Angeles" }
+                else {$("#NYCPic").show(), result = "New York"}
+              }
             }
-            else { //classy
-              if (temperature === "hot") {result = "a remote Florida key to go jetskiing" }
-              else {result = "the Oregon coast for drysuit diving"}
-            }
-          }
-        }
-        else { //mountain
-          $("#mountainPic").fadeOut(500)
-          $("#waterPic").fadeOut(500)
-          setTimeout(function() {
-            $("#mountainPic").fadeIn(500);
-          }, 500)
-          if (density === "city") {
-            if (price === "cheap") {
-              if (temperature === "hot") {result = "Phoenix" }
-              else {result = "Salt Lake City"}
-            }
-            else { //classy
-              if (temperature === "hot") {result = "Las Vegas" } //Turns out Googling "classy hot mountain city" gives some unexpected results.
-              else {result = "Denver"}
+
+            else { //rural
+              if (price === "cheap") {
+                if (temperature === "hot") {$("#evergladesPic").show(), result = "the Everglades" }
+                else {$("#allagashPic").show(), result = "the Allagash Wilderness"}
+              }
+              else { //classy
+                if (temperature === "hot") {$("#jetskiPic").show(), result = "a remote Florida key to go jetskiing" }
+                else {$("#drysuitPic").show(), result = "the Oregon coast for drysuit diving"}
+              }
             }
           }
-          else { //rural
-            if (price === "cheap") {
-              if (temperature === "hot") {result = "Guadalupe National Park" }
-              else {result = "Mount Hood"}
+          else { //mountain
+            if (density === "city") {
+              if (price === "cheap") {
+                if (temperature === "hot") {$("#phoenixPic").show(), result = "Phoenix" }
+                else {$("#SLCPic").show(), result = "Salt Lake City"}
+              }
+              else { //classy
+                if (temperature === "hot") {$("#vegasPic").show(), result = "Las Vegas" } //Turns out Googling "classy hot mountain city" gives some unexpected results.
+                else {$("#denverPic").show(), result = "Denver"}
+              }
             }
-            else { //classy
-              if (temperature === "hot") {result = "a guided tour of the Grand Canyon" }
-              else {result = "Aspen for a snowmobile adventure"}
+            else { //rural
+              if (price === "cheap") {
+                if (temperature === "hot") {$("#guadaPic").show(), result = "Guadalupe National Park" }
+                else {$("#hoodPic").show(), result = "Mount Hood"}
+              }
+              else { //classy
+                if (temperature === "hot") {$("#canyonPic").show(), result = "a guided tour of the Grand Canyon" }
+                else {$("#aspenPic").show(), result = "Aspen for a snowmobile adventure"}
+              }
             }
           }
-        }
-      }
+        }, 500) //ends delay function making fadeout work for pictures and such
+      }//ends nested if for domestic travel
       else {  //foreign travel
         $("#domesticResult").fadeOut(500)
         $("#foreignResult").fadeOut(500)
@@ -85,9 +77,8 @@ $(document).ready(function() {
       setTimeout(function() {
         $("#output").text(result);
       }, 500)
-
     }
-    else {
+    else { //for empty input boxes
       $("#inputError").show();
       $(".has-error").removeClass("has-error");
       if (!travel) {$("#travelInputError").addClass("has-error")}
